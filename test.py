d6542c9d6049f82b82702ed907f294527db840c0
Carmen <54728377+crfews@users.noreply.github.com>
2022-05-11 14:03:29 -0400

    Create test.py
    short test file

---
+++ test.py
@@ -0,0 +1,21 @@
+import jobanalysis
+
+# create sample list of jobs
+url_list = [
+    'https://www.linkedin.com/jobs/view/2959124718',
+    'https://www.linkedin.com/jobs/view/3030646645',
+    'https://www.linkedin.com/jobs/view/3023688838',
+    'https://www.linkedin.com/jobs/view/3042583942',
+    'https://www.linkedin.com/jobs/view/3042428478',
+    'https://www.linkedin.com/jobs/view/3036847597',
+    'https://www.linkedin.com/jobs/view/3041899464',
+    'https://www.linkedin.com/jobs/view/3044949120',
+    'https://www.linkedin.com/jobs/view/3039549560'
+]
+
+jobs = Cloud_Job_Analysis(url_list) # create cloud object
+
+jobs.evaluate()
+
+jobs.make_cloud()
+
