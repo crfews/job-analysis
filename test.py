db678267688372070361f7670a2e7b3d2f37b037
crfews <54728377+crfews@users.noreply.github.com>
2022-05-17 14:00:04 -0400

    remove save figure and update output files

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ jobs.evaluate(createimg=False,pos=['ADV','NOUN','VERB'])
 
 job_data = jobs.job_data
 
-jobs.make_cloud(saveplot=True)
+jobs.make_cloud(saveplot=False)
 
 print(job_data)
 job_data.to_excel('job_data.xlsx')
