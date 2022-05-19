2972f53b879effc6f748a5222c09d3aa944ef5e6
crfews <54728377+crfews@users.noreply.github.com>
2022-05-18 21:30:41 -0400

    adding new test file for logistics jobs

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import jobanalysis
 import pandas as pd
 
-# create sample list of jobs
+# create sample list of data science jobs
 url_list = [
     'https://www.linkedin.com/jobs/view/2959124718',
     'https://www.linkedin.com/jobs/view/3030646645',
@@ -11,7 +11,10 @@ url_list = [
     'https://www.linkedin.com/jobs/view/3036847597',
     'https://www.linkedin.com/jobs/view/3041899464',
     'https://www.linkedin.com/jobs/view/3044949120',
-    'https://www.linkedin.com/jobs/view/3039549560'
+    'https://www.linkedin.com/jobs/view/3039549560',
+    'https://www.linkedin.com/jobs/view/3073790154',
+    'https://www.linkedin.com/jobs/view/3072066702',
+    'https://www.linkedin.com/jobs/view/3076238941'
 ]
 
 jobs = jobanalysis.Cloud_Job_Analysis(url_list) # create cloud object
@@ -20,7 +23,7 @@ jobs.evaluate(createimg=False,pos=['ADV','NOUN','VERB'])
 
 job_data = jobs.job_data
 
-jobs.make_cloud(saveplot=True)
+jobs.make_cloud(saveplot=False)
 
 print(job_data)
 job_data.to_excel('job_data.xlsx')
---
+++ test2.py
@@ -0,0 +1,26 @@
+import jobanalysis
+import pandas as pd
+
+# create sample list of logistics jobs
+url_list = [
+    'https://www.linkedin.com/jobs/view/3063429466',
+    'https://www.linkedin.com/jobs/view/3019370404',
+    'https://www.linkedin.com/jobs/view/2829993099',
+    'https://www.linkedin.com/jobs/view/3074129179',
+    'https://www.linkedin.com/jobs/view/2828387493',
+    'https://www.linkedin.com/jobs/view/3075098988',
+    'https://www.linkedin.com/jobs/view/3068137055',
+    'https://www.linkedin.com/jobs/view/3072626331',
+    'https://www.linkedin.com/jobs/view/3074267030'
+]
+
+jobs = jobanalysis.Cloud_Job_Analysis(url_list) # create cloud object
+
+jobs.evaluate(createimg=False,pos=['ADJ','ADV','NOUN'])
+
+job_data = jobs.job_data
+
+jobs.make_cloud(saveplot=False)
+
+print(job_data)
+job_data.to_excel('logistics_job_data.xlsx')
